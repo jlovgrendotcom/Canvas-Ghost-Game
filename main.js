a6cd00b811aa66c39e7a31cd51b8cd95ed60e622
Hnicoulin <82177528+Hnicoulin@users.noreply.github.com>
2021-06-18 17:30:39 -0700

    Update main.js
    Ghosts now moving.

--- main.js
+++ main.js
@@ -12,47 +12,72 @@ images.fghost = new Image();
 images.ghost.src = 'Ghost.png';
 images.pghost.src = 'PeacefulGhost.png';
 images.fghost.src = 'FastGhost.png';
-const ghostsActions = ['up','right', 'down right', 'down']
+//const ghostsActions = ['up','right', 'down right', 'down']
+const ghostsActions = ['up','right'];
+const numberofGhosts = 2;
 const ghosts = [];
 
 
 class Ghost {
   constructor(){
-    this.width = 180.0625;
-    this.height = 113.125;
-    this.frameX = 0;
+    this.height = 197; //h & w of sprite on sprite sheet
+    this.width = 128;
+    this.frameX = 0; //cordinates on spirte sheet so we know which image
     this.frameY =0;
-    this.x =0;
-    this.y =0;
-    this.speed = (Math.random()* 1.5)+3.5;
-    
+    this.x =Math.random()* canvas.width; // coordinates for where we want the sprite on the background image
+    this.y =Math.random()* canvas.height;//use math.random so the ghost arent on top of each other
+    this.speed = 6;
+    this.action = ghostsActions [Math.floor(Math.random()* ghostsActions.length)]; // randomnizes the index selction of which direction the ghost will go. Math.floor rounds down to an even number.
+    if(this.action === 'up'){
+         this.frameY = 3;
+       
+    }else {
+      this.frameY =1;
+      this.height = 290; //The sprite sheet is uneven so I have to change the height and width depending on which frame/direction I want
+    this.width = 130;
+    }
   }
   draw(){
     drawGhost(images.ghost,this.width * this.frameX,this.height *this.frameY,this.width, this.height,this.x,this.y,this.width, this.height);
 
     if (this.frameX < 3) {this.frameX++;
    }else{
-    this.frameX = 2;
+    this.frameX = 3;
   }
 
   }
-  move(){
+  move(){ 
    if(this.action === 'right'){
-       // move ghost
-  if (this.x < canvas.width + this.width){
-    this.x += this.speed;
+      
+    if (this.x > canvas.width + this.width){
+     this.x = 0 - this.width;
+      this.y = Math.random()* (canvas.height - this.height);
+        //randomly appears on the background. Minus the ghodt height so that it doesnt spawn off screen
+    
+    }
+    else{
+      // move ghost
+      this.x += this.speed;
+    }
+  }else if (this.action === 'up' ){
+    
+    if (this.y < (0 -this.height)){
+      this.y = canvas.height + this.height;
+      this.x = Math.random() * canvas.width;
+      
+    }else{ //ghost has not walked off the screen yet. substracting its location by its speed it will make it go off the screen gradually.
+      this.y -=this.speed;
+    }
   }
-  else{
-    this.x = 0 - this.width;
-   }
-  }
+}
 }
 
 
 // create variables to find a specific sprite in the sheet
-
-
+for (i =0; i < numberofGhosts; i++){
 ghosts.push(new Ghost());
+}
+
 
 //first four parameters are from the image source and specfiy what needs to be cropped. The second four parameters are for where the image should go on the canvas.
 function drawGhost(img, sX,sY,sW,sH,dX,dY,dW,dH){
@@ -63,8 +88,11 @@ function drawGhost(img, sX,sY,sW,sH,dX,dY,dW,dH){
 
 function animateGhost(){
   ctx.clearRect(0,0,canvas.width, canvas.height);
-  ghosts[0].draw();
-  ghosts[0].move();
+  for (i = 0; i < ghosts.length; i++ ){
+    ghosts[i].draw();
+  ghosts[i].move();
+  }
+  
  /* drawSprite(images.ghost,ghostWidth * ghostFrameX,ghostHeight *ghostFrameY,ghostWidth, ghostHeight,ghostX,ghostY,ghostWidth, ghostHeight);
 
   if (ghostFrameX < 3) {ghostFrameX++;
